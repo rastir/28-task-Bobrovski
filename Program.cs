e7bdf7a63c9942ab3951205de3ed35f3de6fe605
Rasul <rastir@yandex.ru>
2021-02-18 19:14:43 +0300

    1 вступительное задание
    Курс Выживший-28 задач

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int Squirrel(int N)
+        {
+            int factorial = 1;
+            if (N != 0)
+            {
+                for (int i = 2; i <= N; i++)
+                {
+                    factorial *= i;
+                }
+                //Console.WriteLine("факториал" + factorial);
+            }
+            if (factorial < 10)
+            {
+                return factorial;
+            }
+            else
+                return factorial - ((factorial % 10) / 10);
+        }
+        static int GetFirstNumber(int number)
+        {
+            if (number < 10)
+            {
+                return number;
+            }
+            return GetFirstNumber((number - (number % 10)) / 10);
+        }
+        static void Main()
+        {
+            int n = 0;
+            Console.Write("Number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            int first = GetFirstNumber(Squirrel(n));
+            Console.WriteLine(first);
+            Console.ReadLine();
+        }
+    }
+}
