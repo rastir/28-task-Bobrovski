2c461edcf91ed8853706fcda72a41e2fec3321b2
Rasul <rastir@yandex.ru>
2021-02-28 15:13:01 +0300

    Update MadMax.cs

--- MadMax.cs
+++ MadMax.cs
@@ -6,32 +6,35 @@ namespace Level1Space
     {
         public static int [] MadMax(int N, int [] Tele)
         {
-            for (int i = 0; i < N - 1; i++)
+            if (N > 1)
             {
-                for (int j = i + 1; j < N; j++)
+                for (int i = 0; i < N - 1; i++)
                 {
-                    if (Tele[i] > Tele[j])
+                    for (int j = i + 1; j < N; j++)
                     {
-                        int b = Tele[i];
-                        Tele[i] = Tele[j];
-                        Tele[j] = b;
+                        if (Tele[i] > Tele[j])
+                        {
+                            int b = Tele[i];
+                            Tele[i] = Tele[j];
+                            Tele[j] = b;
+                        }
                     }
                 }
-            }
-            decimal P = (decimal)N / 2; ;
-            int M = (int)Math.Round(P);
-            for (int i = M - 1; i < N; i++)
-            {
-                for (int j = i; j < N - 1; j++)
+                decimal P = (decimal)N / 2; ;
+                int M = (int)Math.Round(P);
+                for (int i = M - 1; i < N; i++)
                 {
-                    if (Tele[i] < Tele[j + 1])
+                    for (int j = i; j < N - 1; j++)
                     {
-                        int b = Tele[i];
-                        Tele[i] = Tele[j + 1];
-                        Tele[j + 1] = b;
+                        if (Tele[i] < Tele[j + 1])
+                        {
+                            int b = Tele[i];
+                            Tele[i] = Tele[j + 1];
+                            Tele[j + 1] = b;
+                        }
                     }
                 }
-            }
+            }            
             return Tele;
         }
     }
