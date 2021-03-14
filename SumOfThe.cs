40a60521c9af3dbc3ba0dc059ea0823d1d451547
Rasul <rastir@yandex.ru>
2021-03-14 19:42:31 +0300

    Update SumOfThe.cs

--- SumOfThe.cs
+++ SumOfThe.cs
@@ -1,1 +1,32 @@
+using System;
 
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static int SumOfThe(int N, int[] data)
+        {
+            int result = 0;
+            int sum = 0;
+            //char[] s1 = s.ToCharArray();
+            //string[,] s3 = new string[s.Length, s.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < data.Length; j++)
+                {
+                    if (i == j)
+                        continue;
+                    else
+                        sum += data[j];
+                }
+                if (data[i] == sum)
+                {
+                    result = sum;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
