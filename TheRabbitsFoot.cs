ba9d96feced4f46414627fa3a241b673f0191833
Rasul <rastir@yandex.ru>
2021-03-31 01:28:44 +0300

    Update TheRabbitsFoot.cs

--- TheRabbitsFoot.cs
+++ TheRabbitsFoot.cs
@@ -6,54 +6,99 @@ namespace Level1Space
     {
         public static string TheRabbitsFoot(string s, bool encode)
         {
-            if (N > 1)
+            if (encode == true)
             {
-                int[,] block = new int[3, 3] { { 6, 1, 9 }, { 5, 2, 8 }, { 4, 3, 7 } };
-                int k1 = 0;
-                int k2 = 0;
-                int k3 = 0;
-                int M = N * 2;
-                int[] coord = new int[M];
-                for (int i = 0; i < M; i++)
+                s = s.Replace(" ", string.Empty);
+                double root = Math.Sqrt(s.Length);
+                int down = (int)Math.Floor(root);
+                int up = (int)Math.Ceiling(root);
+                if (down * up < s.Length)
+                    down++;
+                string[,] matrix = new string[up, down];
+                int k = 0;
+                for (int i = 0; i < up; i++)
                 {
-                    for (int j = 0; j < 3; j++)
+                    for (int j = 0; j < down; j++)
                     {
-                        if (hits[k3] == block[k2, j])
+                        if (k < s.Length)
                         {
-                            coord[k1] = k2;
-                            k1++;
-                            coord[k1] = j;
-                            k1++;
-                            k3++;
-                            k2 = 0;
-                            break;
+                            matrix[i, j] = s[k].ToString();
+                            k++;
+                            Console.Write(matrix[i, j]);
                         }
-                    }
-                    if (k2 == 2)
-                        k2 = 0;
-                    else
-                        k2++;
-                    if (k3 == N)
-                        break;
-                }
-                double summ = 0;
-                for (int i = 0; i < M - 2; i++)
-                {
-                    if (i % 2 == 0)
-                    {
-                        if ((Math.Abs(coord[i] - coord[i + 2]) + Math.Abs(coord[i + 1] - coord[i + 3])) == 1)
-                            summ++;
                         else
-                            summ += 1.414213;
+                            break;
                     }
+                    Console.WriteLine();
                 }
-                summ = Math.Round(summ, 5);
-                string summ2 = summ.ToString(format: "0.00000000");
-                summ2 = summ2.Replace(@",", "").Replace(@"0", "");
-                return summ2;
+                int p = down * up;
+                k = 0;
+                string result = "";
+                for (int i = 0; i < down; i++)
+                {
+                    for (int j = 0; j < up; j++)
+                    {
+                        if (k < p)
+                        {
+                            result += matrix[j, i];
+                            k++;
+                        }
+                        else
+                            break;
+                    }
+                    result += " ";
+                }
+                return result;
             }
             else
-                return "0";
+            {
+                string s1 = s.Replace(" ", string.Empty);
+                double root = Math.Sqrt(s1.Length);
+                int down = (int)Math.Floor(root);
+                int up = (int)Math.Ceiling(root);
+                if (down * up < s1.Length)
+                    down++;
+                string[,] matrix = new string[up, down];
+                int k = 0;
+                s = s.Trim();
+                char[] array = s.ToCharArray();
+                for (int i = 0; i < up; i++)
+                {
+                    for (int j = 0; j < down; j++)
+                    {
+                        if (k < down * up) 
+                        {
+                            matrix[i, j] = array[k].ToString();
+                            if ((array[k].ToString() == " ") && (j == down - 1))
+                                break;
+                            else if (array[k].ToString() == " ")
+                            {
+                                k++;
+                                matrix[i, j] = array[k].ToString();
+                            }
+                            k++;
+                        }
+                        else
+                            break;
+                    }
+                }
+                k = 0;
+                string result = "";
+                for (int i = 0; i < down; i++)
+                {
+                    for (int j = 0; j < up; j++)
+                    {
+                        if (k < s.Length)
+                        {
+                            result += matrix[j, i];
+                            k++;
+                        }
+                        else
+                            break;
+                    }
+                }
+                return result;
+            }
         }
     }
 }
