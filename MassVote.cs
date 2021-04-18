719e262e8778c12a2d371f6d6f1e52e917987e0d
Rasul <rastir@yandex.ru>
2021-04-18 18:58:39 +0300

    Update MassVote.cs

--- MassVote.cs
+++ MassVote.cs
@@ -1,1 +1,44 @@
+using System;
 
+namespace Level1Space
+{
+    public static class Level1
+    {
+        public static string MassVote(int N, int[] Votes)
+        {
+            int max = Votes[0];
+            int x=0;
+            for (int i = 0; i < Votes.Length; i++) 
+            {
+                if (Votes[i] > max)
+                {
+                    max = Votes[i];
+                    x = i;
+                }
+            }
+            x++;
+            int k = 0;
+            for (int i = 0; i < Votes.Length; i++) 
+            {
+                if (Votes[i] == max)
+                    k++;  
+            }
+            if (k > 1)
+                return "no winner";
+            else
+            {
+                int summ = 0;
+                for (int i=0; i < Votes.Length; i++)
+                {
+                    summ += Votes[i];
+                }
+                decimal summ2 = 100 * max / summ;
+                summ2 = Math.Round(summ2, 3);
+                if (summ2 > 50)
+                    return $"majority winner {x}";
+                else
+                    return $"minority winner {x}";
+            }
+        }
+    }
+}
