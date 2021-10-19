55b3c0014cabc36a1b8ea05c4b05b8ea378bd5a8
Rasul <rastir@yandex.ru>
2021-10-20 00:58:42 +0300

    Update MisterRobot.cs

--- MisterRobot.cs
+++ MisterRobot.cs
@@ -20,43 +20,59 @@ namespace Level1Space
 
                 while ((!sorted) || timeOver == true)
                 {
-                    Random rnd = new Random();
                     sorted = true;
-                    int i = rnd.Next(0, data.Length - 1);
-                    if ((i + 2) > (data.Length - 1))
-                        i = data.Length - 3;
 
-                    a = data[i];
-                    b = data[i + 1];
-                    c = data[i + 2];
-
-                    int count = 0;
-                    while (!(a <= b && b <= c) || timeOver == true || count == 3)
+                    for (int i = data.Length - 1; i > 1; i--)
                     {
-                        tmp = a; 
-                        a = b; 
-                        b = c; 
-                        c = tmp;
-
-                        count++;
-                        EndTime = DateTime.UtcNow;
-                        diff = EndTime - StartTime;
-                        differenceInSecond = diff.TotalSeconds;
-                        if (differenceInSecond > 1)
-                        {
-                            timeOver = true;
-                            sorted = false;
-                            break;
-                        }
-                        else
+                        if (data[i] > data[i - 1])
                             continue;
-                    }
-                    if (timeOver == true)
-                        break;
-                    data[i] = a;
-                    data[i + 1] = b;
-                    data[i + 2] = c;
+                        else
+                        {
+                            a = data[i];
+                            b = data[i - 1];
+                            c = data[i - 2];
 
+                            int count = 0;
+                            while (!(c <= b && b <= a) || timeOver == true)
+                            {
+                                tmp = c;
+                                c = b;
+                                b = a;
+                                a = tmp;
+
+                                count++;
+                                if (count == 3)
+                                {
+                                    while (!(b < a))
+                                    {
+                                        tmp = c;
+                                        c = b;
+                                        b = a;
+                                        a = tmp;
+                                    }
+                                    break;
+                                }
+
+                                EndTime = DateTime.UtcNow;
+                                diff = EndTime - StartTime;
+                                differenceInSecond = diff.TotalSeconds;
+                                if (differenceInSecond > 1)
+                                {
+                                    timeOver = true;
+                                    sorted = false;
+                                    break;
+                                }
+                                else
+                                    continue;
+                            }
+                            if (timeOver == true)
+                                break;
+                            data[i] = a;
+                            data[i - 1] = b;
+                            data[i - 2] = c;
+                            sorted = false;
+                        }
+                    }
                     for (int j = 0; j < data.Length - 1; j++)
                     {
                         if (data[j] > data[j + 1])
@@ -69,7 +85,6 @@ namespace Level1Space
                     differenceInSecond = diff.TotalSeconds;
                     if (differenceInSecond > 1)
                     {
-                        timeOver = true;
                         break;
                     }
                     else
