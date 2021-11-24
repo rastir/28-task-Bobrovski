b93401097a4681c27ebc7a8e4f78790c26d643fb
Rasul <rastir@yandex.ru>
2021-11-24 20:42:20 +0300

    Update TransformTransform.cs

--- TransformTransform.cs
+++ TransformTransform.cs
@@ -5,6 +5,36 @@ namespace Level1Space
 {
     public static class Level1
     {
-       return true; 
+       public static bool TransformTransform(int[] A, int N)
+        {
+            int[] B = new int[N];
+            int k = 0;
+            int r = N;
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = 0; j < N - i - 1; j++)
+                {
+                    k = i + j;
+                    int maxValue = int.MinValue;
+                    for (int x = j; x < k; x++)
+                    {
+                        if (A[x] > maxValue)
+                        {
+                            // найден больший элемент
+                            maxValue  = A[x];
+                        }
+                    }
+                    if (r > 0)
+                        B[r - 1] = maxValue;
+                    else if (r == 0)
+                        B[r] = maxValue;
+                    if (r > 0)
+                        r--;
+                }
+            }
+
+            return true;
+        }
     }
 }
