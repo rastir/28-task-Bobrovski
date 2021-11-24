0a1fd92adfb5f2197193af7c4b1ef8ae0c079856
Rasul <rastir@yandex.ru>
2021-11-24 20:49:07 +0300

    Update TransformTransform.cs

--- TransformTransform.cs
+++ TransformTransform.cs
@@ -10,6 +10,7 @@ namespace Level1Space
             int[] B = new int[N];
             int k = 0;
             int r = N;
+            int key;
 
             for (int i = 0; i < N - 1; i++)
             {
@@ -34,7 +35,43 @@ namespace Level1Space
                 }
             }
 
-            return true;
+            int[] C = new int[N];
+            int k2 = 0;
+            int r2 = N;
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = 0; j < N - i - 1; j++)
+                {
+                    k2 = i + j;
+                    int maxValue = int.MinValue;
+                    for (int x = j; x < k; x++)
+                    {
+                        if (B[x] > maxValue)
+                        {
+                            // найден больший элемент
+                            maxValue = B[x];
+                        }
+                    }
+                    if (r > 0)
+                        C[r - 1] = maxValue;
+                    else if (r == 0)
+                        C[r] = maxValue;
+                    if (r > 0)
+                        r--;
+                }
+            }
+
+            int summ = 0;
+            for (int i = 0; i < N; i++)
+            {
+                summ += C[i];
+            }
+
+            if (summ % 2 == 0)
+                return true;
+            else
+                return false;
         }
     }
 }
